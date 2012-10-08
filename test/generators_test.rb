fa62048a3563c4385715156390a477a98047b438
twinturbo <me@broadcastingadam.com>
2012-10-09 00:55:06 +0200

    Refactor Generators test

--- test/generators_test.rb
+++ test/generators_test.rb
@@ -1,14 +1,6 @@
 require "test_helper"
 
 class Hydrogen::GeneratorsTest < MiniTest::Unit::TestCase
-  def invoke(*args)
-    Hydrogen::Generators.invoke *args
-  end
-
-  def teardown
-    Hydrogen::Generators.subclasses.clear
-  end
-
   def test_generator_lookup_against_full_name
     generator = Class.new do
       def self.full_name
@@ -20,7 +12,9 @@ class Hydrogen::GeneratorsTest < MiniTest::Unit::TestCase
 
     generator.expects(:start).returns(:invoked)
 
-    result = invoke "foo"
+    result = Hydrogen::Generators.invoke "foo"
     assert_equal :invoked, result
+  ensure
+    Hydrogen::Generators.subclasses.clear
   end
 end
