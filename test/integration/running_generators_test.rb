ea6b5111c9bd3710d3b8f7fc8c8625a0772cc855
twinturbo <me@broadcastingadam.com>
2012-10-09 02:25:51 +0200

    Rename test classes

---
+++ test/integration/running_generators_test.rb
@@ -0,0 +1,35 @@
+require 'test_helper'
+
+class Hydrogen::RunningGeneratorsTest < MiniTest::Unit::TestCase
+  def test_generators_are_invoked_with_the_proper_args
+    generator = Class.new Hydrogen::Generator do
+      def self.full_name
+        "foo"
+      end
+    end
+
+    generator.expects(:start).with(["bar", "baz"], {}).returns(:invoked)
+
+    cli = Class.new Hydrogen::CLI
+
+    result = cli.start ["g", "foo", "bar", "baz"]
+
+    assert_equal :invoked, result
+  end
+
+  def test_switches_are_passed_to_generator
+    generator = Class.new Hydrogen::Generator do
+      def self.full_name
+        "foo"
+      end
+    end
+
+    generator.expects(:start).with(["bar", "--quick"], {}).returns(:invoked)
+
+    cli = Class.new Hydrogen::CLI
+
+    result = cli.start ["g", "foo", "bar", "--quick"]
+
+    assert_equal :invoked, result
+  end
+end
